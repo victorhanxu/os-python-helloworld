5a81bf955eeb114299c1440e96d69425e658310c
Victor Han <45407000+victorhanxu@users.noreply.github.com>
2020-05-12 19:19:30 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello from Openshift!"
+    return "This is my Openshift App!"
 
 if __name__ == "__main__":
     application.run()
